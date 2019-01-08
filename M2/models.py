60446057598abb0f585c655bfd0418cdaf0868d9
HironoOkamoto <you@example.com>
2019-01-08 11:28:51 -0500

    m1 m2 inprogress, m1 success

--- M2/models.py
+++ M2/models.py
@@ -7,6 +7,7 @@ from pixyz.distributions import Normal, Bernoulli, RelaxedCategorical, Categoric
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# q(z|x, y)
 class Encoder(Normal):
     def __init__(self, z_dim=63, y_dim=10):
         super(Encoder, self).__init__(cond_var=["x", "y"], var=["z"], name="q")
@@ -47,7 +48,7 @@ class Encoder(Normal):
         scale = F.softplus(x[:, self.z_dim:])
         return {"loc": mu, "scale": scale}
 
-
+# p(x|z, y)
 class Decoder(Bernoulli):
     def __init__(self, z_dim=63, y_dim=10):
         super(Decoder, self).__init__(cond_var=["z", "y"], var=["x"])
@@ -81,6 +82,66 @@ class Decoder(Bernoulli):
         z1 = self.fc1(z)
         z2 = self.fc2(z)
         z = torch.cat([z1, z2*y], dim=1)
+        h = self.fc_d(z)
+        h = h.view(-1, 128, 7, 7)
+        return {"probs": self.conv_d(h)}
+    
+# q(z|x)
+class Encoder_m1(Normal):
+    def __init__(self, z_dim=63):
+        super(Encoder_m1, self).__init__(cond_var=["x"], var=["z"], name="q")
+
+        self.z_dim = z_dim
+
+        # encode
+        self.conv_e = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),  # 28 ⇒ 14
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 14 ⇒ 7
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2),
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(128 * 7 * 7, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2*self.z_dim),
+        )
+
+    def forward(self, x):
+        x = self.conv_e(x)
+        x = x.view(-1, 128 * 7 * 7)
+        x = self.fc(x)
+        mu = x[:, :self.z_dim]
+        scale = F.softplus(x[:, self.z_dim:])
+        return {"loc": mu, "scale": scale}
+
+# p(x|z)
+class Decoder_m1(Bernoulli):
+    def __init__(self, z_dim=63):
+        super(Decoder_m1, self).__init__(cond_var=["z"], var=["x"])
+        
+        self.z_dim = z_dim 
+
+        # decode
+        self.fc_d = nn.Sequential(
+            nn.Linear(self.z_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 128 * 7 * 7),
+            nn.LeakyReLU(0.2)
+        )
+        self.conv_d = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, z):
         h = self.fc_d(z)
         h = h.view(-1, 128, 7, 7)
         return {"probs": self.conv_d(h)}
@@ -121,6 +182,85 @@ class Classifier(RelaxedCategorical):
         c2_flat = c2.view(c2.size(0), -1)
         out = self.fc(c2_flat)
         return {"probs": out}
+    
+# latent classifier p(y|z)
+class LatentClassifier(RelaxedCategorical):    
+    def __init__(self, z_dim=63, y_dim=10):
+        super(LatentClassifier, self).__init__(cond_var=["z"], var=["y"], temperature=0.5)
+        self.fc =  nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.Dropout(p=0.4),
+            nn.ReLU(),
+            nn.Linear(256, y_dim),
+            nn.Dropout(p=0.4),
+            nn.Softmax(dim=1)            
+        )
+        initialize_weights(self)
+        
+    def forward(self, z):
+        out = self.fc(z)
+        return {"probs": out}
+    
+# q(z_2|z_1, y)
+class LatentEncoder(Normal):
+    def __init__(self, z_dim=63, z2_dim=20, y_dim=10):
+        super(LatentEncoder, self).__init__(cond_var=["z", "y"], var=["z2"], name="q")
+        self.z_dim = z_dim
+        self.z2_dim = z2_dim
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(z_dim,  40),
+        )      
+        self.fc2 = nn.Sequential(
+            nn.Linear(z_dim,  y_dim),
+        )                
+        self.fc = nn.Sequential(
+            nn.Linear(40+y_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2*z2_dim),
+        )
+
+    def forward(self, z, y):
+        z1 = self.fc1(z)
+        z2 = self.fc2(z)
+        z = torch.cat([z1, z2*y], dim=1)
+        z = self.fc(z)
+        mu = z[:, :self.z2_dim]
+        scale = F.softplus(z[:, self.z2_dim:])
+        return {"loc": mu, "scale": scale}
+    
+# p(z_1|z_2, y)
+class LatentDecoder(Normal):
+    def __init__(self, z_dim=63, z2_dim=20, y_dim=10):
+        super(LatentDecoder, self).__init__(cond_var=["z2", "y"], var=["z"])
+        
+        self.z_dim = z_dim 
+        self.z2_dim = z2_dim 
+
+        # decode
+        self.fc1 = nn.Sequential(
+            nn.Linear(z2_dim, 40),
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(z2_dim, y_dim),
+        )
+        
+        self.fc_d = nn.Sequential(
+            nn.Linear(40+y_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, z_dim*2),
+        )
+        
+    def forward(self, z, y):
+        z1 = self.fc1(z)
+        z2 = self.fc2(z)
+        z = torch.cat([z1, z2*y], dim=1)
+        z = self.fc_d(z)
+        mu = z[:, :self.z2_dim]
+        scale = F.softplus(z[:, self.z2_dim:])
+        return {"loc": mu, "scale": scale}
     
 def initialize_weights(model):
     for m in model.modules():
--- M2/utils.py
+++ M2/utils.py
@@ -46,6 +46,62 @@ def plot_sample(D, epoch, z_dim=63, y_dim=10, sample_num=100):
     plt.savefig("./logs/mnist_gif/{}.png".format(epoch+1), bbox_inches="tight", pad_inches=0.0)
     plt.show()
     
+def plot_sample_m1(D, epoch, z_dim=63, y_dim=10, sample_num=100):
+    # fixed noise & condition サンプル画像を作る用
+    np.random.seed(42)
+    sample_z_ = torch.from_numpy(np.random.randn(sample_num, z_dim).astype("float32")).to(device)
+    samples = D(sample_z_)
+    samples = samples["probs"].cpu().data.numpy().squeeze()
+    
+    # 横軸 z固定, y変化
+    # 縦軸 z変化, y固定
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(100):
+        plt.subplot(10, 10, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(samples[i], cmap=plt.cm.gray)
+    fig.suptitle("epoch: {}".format(epoch+1), va="bottom", fontsize=30, y=0.9)
+    plt.savefig("./logs/mnist_gif_m1/{}.png".format(epoch+1), bbox_inches="tight", pad_inches=0.0)
+    plt.show()
+    
+def plot_sample_m1m2(D2, D1, epoch, z_dim=63, y_dim=10, sample_num=100):
+    # fixed noise & condition サンプル画像を作る用
+    np.random.seed(42)
+    sample_z_ = torch.zeros((sample_num, z_dim))
+    for i in range(10):
+        sample_z_[i*y_dim] = torch.from_numpy(np.random.randn(1, z_dim))#torch.rand(1, z_dim) # 正規分布randn or 一様分布rand
+        for j in range(1, y_dim):
+            sample_z_[i*y_dim + j] = sample_z_[i*y_dim]
+
+    temp = torch.zeros((10, 1))
+    for i in range(y_dim):
+        temp[i, 0] = i
+
+    temp_y = torch.zeros((sample_num, 1))
+    for i in range(10):
+        temp_y[i*y_dim: (i+1)*y_dim] = temp
+
+    sample_y_ = torch.zeros((sample_num, y_dim))
+    sample_y_.scatter_(1, temp_y.type(torch.LongTensor), 1)
+    sample_z_, sample_y_ = sample_z_.to(device), sample_y_.to(device)
+
+    z = D2.sample_mean(sample_z_, sample_y_)
+    samples = D1(z)
+    samples = samples["probs"].cpu().data.numpy().squeeze()
+    
+    # 横軸 z固定, y変化
+    # 縦軸 z変化, y固定
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(100):
+        plt.subplot(10, 10, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(samples[i], cmap=plt.cm.gray)
+    #plt.savefig("./logs/generate.png")
+    fig.suptitle("epoch: {}".format(epoch+1), va="bottom", fontsize=30, y=0.9)
+    plt.savefig("./logs/mnist_gif_m1m2/{}.png".format(epoch+1), bbox_inches="tight", pad_inches=0.0)
+    plt.show()
     
 def plot_loss(train_hist):
 #     plt.figure(figsize=(10, 10))
@@ -72,6 +128,28 @@ def compute_precision(C, test_loader):
         for batch_idx, (x, y) in enumerate(test_loader):
             x = x.to(device)
             pred = C(x)
+            preds.extend(pred["probs"].detach().cpu().numpy())
+            ys.extend(y)
+    preds = np.array(preds)
+    ys = np.array(ys)
+            
+    precision = precision_score(ys, preds.argmax(1), average="macro")
+    print(precision)
+    print(confusion_matrix(ys, preds.argmax(1)))
+    print(classification_report(ys, preds.argmax(1)))
+    return precision
+
+# test
+def compute_precision_m1(C, E, test_loader):
+    C.eval()
+    E.eval()
+    preds = []
+    ys = []
+    with torch.no_grad():
+        for batch_idx, (x, y) in enumerate(test_loader):
+            x = x.to(device)
+            z = E.sample_mean({"x": x})
+            pred = C(z)
             preds.extend(pred["probs"].detach().cpu().numpy())
             ys.extend(y)
     preds = np.array(preds)
