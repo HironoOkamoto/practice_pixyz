43c8911c5cdf7a0a58e7a7f39a5f745b5ecb18f0
HironoOkamoto <you@example.com>
2019-01-14 22:05:05 -0500

    training code

--- factorVAE/utils.py
+++ factorVAE/utils.py
@@ -1,3 +1,5 @@
+import os
+from os.path import exists, join
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
@@ -34,7 +36,7 @@ def encoder_plot(data_loader, E, D):
     plt.show()
     print("↑true")
     
-def traverse_plot(data_loader, E, D, n, m = 11, scale = 5, z_dim=10):
+def traverse_plot(data_loader, E, D, n, m=11, scale=5, z_dim=10):
     E.eval()
     D.eval()
 
@@ -70,9 +72,56 @@ def traverse_plot(data_loader, E, D, n, m = 11, scale = 5, z_dim=10):
         plt.yticks([])
         plt.subplots_adjust(wspace=0., hspace=0.)
         if i < m:
-            plt.title("{:+}".format(int(scales[i])), fontsize=15)
+            plt.title("{:+.1f}".format(scales[i]), fontsize=15)
         if i%m == 0:
             plt.ylabel("z{}".format(int(i/m+1)), fontsize=15)
         plt.imshow(samples_[i], cmap=plt.cm.gray)
 
     plt.show()
+    
+def make_gif(n, data_loader, E, D, name, m=30, scale=3, z_dim=10):
+    E.eval()
+    D.eval()
+
+    images = iter(data_loader).next()
+    images = images[n:n+1].to(device)
+    z = E.sample_mean({"x": images})
+    samples = D.sample_mean({"z": z})
+    images_ = images.cpu().data.numpy().transpose(0, 2, 3, 1).squeeze()
+    samples_ = samples.cpu().data.numpy().transpose(0, 2, 3, 1).squeeze()
+    plt.subplot(121)
+    plt.title("original")
+    plt.imshow(images_, plt.cm.gray)
+    plt.subplot(122)
+    plt.title("reconstruction")
+    plt.imshow(samples_, plt.cm.gray)
+    plt.show()
+
+    zeros = torch.zeros(z_dim*m, z_dim)
+    scales = np.linspace(-scale, scale, m)
+    for i, v1 in enumerate(scales):
+        for j in range(z_dim):
+            zeros[i*z_dim+j][j] = v1
+
+    z_repeat = z.squeeze().repeat(z_dim*m).view(-1, z_dim)
+    z_ = z_repeat + zeros.to(device)
+    samples = D(z_)
+    samples_ = samples["probs"].cpu().data.numpy().transpose(0, 2, 3, 1).squeeze()
+
+    gif_path = "./logs/{}_gif_{}".format(name, n)
+    if not exists(gif_path):
+        os.makedirs(gif_path)
+
+    for j in range(m):
+        plt.figure(figsize=(z_dim, 1))
+        for i in range(z_dim):
+            plt.subplot(1, z_dim, i+1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.subplots_adjust(wspace=0., hspace=0.)
+            plt.title("z{}".format(int(i+1)), fontsize=15)
+            if i==0:
+                plt.ylabel("{:+.1f}".format(scales[j]), fontsize=15)
+            plt.imshow(samples_[i+10*j], cmap=plt.cm.gray)
+        plt.savefig(join(gif_path, "{}.png".format(j)))
+        plt.show()
