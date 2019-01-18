e0a28e6ca91ab877cee9457f1e3230d5a9b2070e
HironoOkamoto <you@example.com>
2019-01-17 23:25:29 -0500

    change dir name

---
+++ FactorVAE/make_gif.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+list=("0" "1" "2")
+for l in ${list[@]}
+do
+    experiment_name="betavae_C_dsprites_z_dim10_gamma80_gif_${l}"
+    input_dir="./logs/${experiment_name}"
+    output_dir="./logs"
+    convert -layers optimize -loop 0 -delay 500 ${input_dir}/*.png ${output_dir}/${experiment_name}.gif
+done
+
