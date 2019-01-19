9f0c79eb0cf9d88a774b462833cae7535f1ed46a
HironoOkamoto <you@example.com>
2019-01-19 18:21:54 -0500

    readme update

--- GAN/make_gif.sh
+++ GAN/make_gif.sh
@@ -3,6 +3,6 @@
 experiment_name="mnist_gif"
 input_dir="./logs/${experiment_name}"
 output_dir="./logs"
-convert -layers optimize -loop 0 -delay 5 ${input_dir}/*.png ${output_dir}/${experiment_name}.gif
+convert -layers optimize -loop 0 -delay 10 ${input_dir}/*.png ${output_dir}/${experiment_name}.gif
 
 
