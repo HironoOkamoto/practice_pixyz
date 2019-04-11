b7deb574c3709804170aec2d80d8ea10f35c3421
HironoOkamoto <you@example.com>
2019-04-11 03:11:32 -0400

    model update

--- JMVAE/models.py
+++ JMVAE/models.py
@@ -5,7 +5,7 @@ import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-from pixyz.distributions import Bernoulli, Normal, Deterministic
+from pixyz.distributions import Normal, Bernoulli, Categorical
 
 class MNIST_A_Classifier(nn.Module):
     def __init__(self):
@@ -123,9 +123,10 @@ class CVAE_Decoder(Bernoulli):
         h = h.view(-1, 128, 8, 8)
         return {"probs": self.conv_d(h)}
     
+    
 class Encoder_XY(Normal):
-    def __init__(self, z_dim=64, y_dim=1+3+4+10):
-        super(Encoder_XY, self).__init__(cond_var=["x", "y"], var=["z"])
+    def __init__(self, z_dim=256, y_dim=1+3+4+10):
+        super(Encoder_XY, self).__init__(cond_var=["x", "y1", "y2", "y3", "y4"], var=["z"])
 
         self.z_dim = z_dim
 
@@ -140,27 +141,59 @@ class Encoder_XY(Normal):
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 16 ⇒ 8
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 64, kernel_size=4, stride=2, padding=1),  # 8 ⇒ 4
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2),            
         )
-        self.fc1 = nn.Sequential(
-            nn.Linear(128 * 8 * 8,  40),
+        self.fc_y1 = nn.Sequential(
+            nn.Linear(1, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 256),                   
         )      
-        self.fc2 = nn.Sequential(
-            nn.Linear(128 * 8 * 8,  y_dim),
+        self.fc_y2 = nn.Sequential(
+            nn.Linear(3, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),           
+            nn.Linear(256, 256),                   
+        )        
+        self.fc_y3 = nn.Sequential(
+            nn.Linear(4, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),     
+            nn.Linear(256, 256),                   
+        )        
+        self.fc_y4 = nn.Sequential(
+            nn.Linear(10, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.2),     
+            nn.Linear(256, 256),            
         )        
         
-        self.fc = nn.Sequential(
-            nn.Linear(40+y_dim, 1024),
-            nn.BatchNorm1d(1024),
+        self.fc_y = nn.Sequential(
+            nn.Linear(1024, 512),
+            nn.BatchNorm1d(512),
             nn.LeakyReLU(0.2),
-            nn.Linear(1024, 2*self.z_dim),
+            nn.Linear(512, 512),
+        )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(1536, 512),
+            nn.BatchNorm1d(512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, z_dim*2),
         )
 
-    def forward(self, x, y):
+    def forward(self, x, y1, y2, y3, y4):
         x = self.conv_e(x)
-        x = x.view(-1, 128 * 8 * 8)
-        x1 = self.fc1(x)
-        x2 = self.fc2(x)
-        x = torch.cat([x1, x2*y], dim=1)
+        x = x.view(-1, 64 * 4 *4)
+        y1 = self.fc_y1(y1)
+        y2 = self.fc_y2(y2)
+        y3 = self.fc_y3(y3)
+        y4 = self.fc_y4(y4)       
+        y = torch.cat([y1, y2, y3, y4], dim=1)
+        y = self.fc_y(y)
+        x = torch.cat([x, y], dim=1)
         x = self.fc(x)
         mu = x[:, :self.z_dim]
         scale = F.softplus(x[:, self.z_dim:])
@@ -168,7 +201,7 @@ class Encoder_XY(Normal):
     
 
 class Encoder_X(Normal):
-    def __init__(self, z_dim=64):
+    def __init__(self, z_dim=256):
         super(Encoder_X, self).__init__(cond_var=["x"], var=["z"])
 
         self.z_dim = z_dim
@@ -202,9 +235,9 @@ class Encoder_X(Normal):
         return {"loc": mu, "scale": scale}
     
 
-class Encoder_Y(Normal):
-    def __init__(self, z_dim=64, y_dim=1+3+4+10):
-        super(Encoder_Y, self).__init__(cond_var=["y"], var=["z"])
+class Encoder_Y1(Normal):
+    def __init__(self, z_dim=256, y_dim=1):
+        super(Encoder_Y1, self).__init__(cond_var=["y1"], var=["z"])
 
         self.z_dim = z_dim
 
@@ -215,15 +248,71 @@ class Encoder_Y(Normal):
             nn.Linear(1024, 2*self.z_dim),
         )
 
-    def forward(self, y):
-        y = self.fc(y)
+    def forward(self, y1):
+        y = self.fc(y1)
         mu = y[:, :self.z_dim]
         scale = F.softplus(y[:, self.z_dim:])
         return {"loc": mu, "scale": scale}
     
+class Encoder_Y2(Normal):
+    def __init__(self, z_dim=256, y_dim=3):
+        super(Encoder_Y2, self).__init__(cond_var=["y2"], var=["z"])
+
+        self.z_dim = z_dim
+
+        self.fc = nn.Sequential(
+            nn.Linear(y_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2*self.z_dim),
+        )
+
+    def forward(self, y2):
+        y = self.fc(y2)
+        mu = y[:, :self.z_dim]
+        scale = F.softplus(y[:, self.z_dim:])
+        return {"loc": mu, "scale": scale}
+
+class Encoder_Y3(Normal):
+    def __init__(self, z_dim=256, y_dim=4):
+        super(Encoder_Y3, self).__init__(cond_var=["y3"], var=["z"])
+
+        self.z_dim = z_dim
+
+        self.fc = nn.Sequential(
+            nn.Linear(y_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2*self.z_dim),
+        )
+
+    def forward(self, y3):
+        y = self.fc(y3)
+        mu = y[:, :self.z_dim]
+        scale = F.softplus(y[:, self.z_dim:])
+        return {"loc": mu, "scale": scale}
+
+class Encoder_Y4(Normal):
+    def __init__(self, z_dim=256, y_dim=10):
+        super(Encoder_Y4, self).__init__(cond_var=["y4"], var=["z"])
+
+        self.z_dim = z_dim
+
+        self.fc = nn.Sequential(
+            nn.Linear(y_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, 2*self.z_dim),
+        )
+
+    def forward(self, y4):
+        y = self.fc(y4)
+        mu = y[:, :self.z_dim]
+        scale = F.softplus(y[:, self.z_dim:])
+        return {"loc": mu, "scale": scale}
 
 class Decoder_X(Bernoulli):
-    def __init__(self, z_dim=64):
+    def __init__(self, z_dim=256):
         super(Decoder_X, self).__init__(cond_var=["z"], var=["x"])
 
         self.z_dim = z_dim
@@ -252,10 +341,10 @@ class Decoder_X(Bernoulli):
         h = h.view(-1, 128, 8, 8)
         return {"probs": self.conv_d(h)}
 
-# Deterministicだとloglikelihoodが計算できずに死ぬ？
-class Decoder_Y(Bernoulli):
-    def __init__(self, z_dim=64, y_dim=18):
-        super(Decoder_Y, self).__init__(cond_var=["z"], var=["y"])
+
+class Decoder_Y1(Bernoulli):
+    def __init__(self, z_dim=256, y_dim=1):
+        super(Decoder_Y1, self).__init__(cond_var=["z"], var=["y1"])
         self.y_dim = y_dim
         self.z_dim = z_dim
 
@@ -270,3 +359,60 @@ class Decoder_Y(Bernoulli):
     def forward(self, z):
         y = self.fc_d(z)
         return {"probs":  torch.sigmoid(y)}
+
+    
+class Decoder_Y2(Categorical):
+    def __init__(self, z_dim=256, y_dim=3):
+        super(Decoder_Y2, self).__init__(cond_var=["z"], var=["y2"])
+        self.y_dim = y_dim
+        self.z_dim = z_dim
+
+        # decode
+        self.fc_d = nn.Sequential(
+            nn.Linear(self.z_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, y_dim),
+        )
+
+    def forward(self, z):
+        y = self.fc_d(z)
+        return {"probs":  torch.softmax(y, dim=1)}
+    
+
+class Decoder_Y3(Categorical):
+    def __init__(self, z_dim=256, y_dim=4):
+        super(Decoder_Y3, self).__init__(cond_var=["z"], var=["y3"])
+        self.y_dim = y_dim
+        self.z_dim = z_dim
+
+        # decode
+        self.fc_d = nn.Sequential(
+            nn.Linear(self.z_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, y_dim),
+        )
+
+    def forward(self, z):
+        y = self.fc_d(z)
+        return {"probs":  torch.softmax(y, dim=1)}
+
+    
+class Decoder_Y4(Categorical):
+    def __init__(self, z_dim=256, y_dim=10):
+        super(Decoder_Y4, self).__init__(cond_var=["z"], var=["y4"])
+        self.y_dim = y_dim
+        self.z_dim = z_dim
+
+        # decode
+        self.fc_d = nn.Sequential(
+            nn.Linear(self.z_dim, 1024),
+            nn.BatchNorm1d(1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, y_dim),
+        )
+
+    def forward(self, z):
+        y = self.fc_d(z)
+        return {"probs":  torch.softmax(y, dim=1)}
